ce5d76830e3a20a5d561748c407e31f04478e323
logan <logan.william.wright@gmail.com>
2022-02-20 20:08:57 +0100

    some small additions

---
+++ Sources/YouEye/Modifiers/Modifiers.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+//public struct Zooming: ViewModifier {
+//    public let max: CGFloat
+//    @Binding
+//    public var zoom: CGFloat
+//    
+//    func body(content: Content) -> some View {
+//        StretchyScrollView(.horizontal) {
+//            content
+//        }
+//        .frame(maxWidth: .infinity)
+//    }
+//}
